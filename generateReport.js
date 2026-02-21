d9d6ea71ff1f80f2520d174b4f3156668296310f
MVisenka <mvisenka@gmail.com>
2026-02-21 17:55:21 +0100

    Update generateReport.js

--- generateReport.js
+++ generateReport.js
@@ -1,12 +1,9 @@
-import chromium from "chrome-aws-lambda";
-import puppeteer from "puppeteer-core";
+import puppeteer from "puppeteer";
 
 export async function generatePdf(html) {
   const browser = await puppeteer.launch({
-    args: chromium.args,
-    defaultViewport: chromium.defaultViewport,
-    executablePath: await chromium.executablePath,
     headless: true,
+    args: ["--no-sandbox", "--disable-setuid-sandbox"]
   });
 
   const page = await browser.newPage();
