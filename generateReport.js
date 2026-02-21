1c70b0b3c9795ffd654d85b1567bb120b8daa975
MVisenka <mvisenka@gmail.com>
2026-02-21 19:24:42 +0100

    Update generateReport.js

--- generateReport.js
+++ generateReport.js
@@ -3,13 +3,22 @@ import puppeteer from "puppeteer";
 export async function generatePdf(html) {
   const browser = await puppeteer.launch({
     headless: true,
-    args: ["--no-sandbox", "--disable-setuid-sandbox"]
+    args: [
+      "--no-sandbox",
+      "--disable-setuid-sandbox",
+      "--disable-dev-shm-usage",
+      "--disable-accelerated-2d-canvas",
+      "--no-first-run",
+      "--no-zygote",
+      "--single-process",
+      "--disable-gpu",
+    ],
   });
 
   const page = await browser.newPage();
   await page.setContent(html, { waitUntil: "networkidle0" });
-
-  const pdf = await page.pdf({ format: "A4", printBackground: true });
+  const pdfBuffer = await page.pdf({ format: "A4" });
   await browser.close();
-  return pdf;
+
+  return pdfBuffer;
 }
